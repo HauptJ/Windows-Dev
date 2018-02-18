7ac060524afd0dac488be081b27a81c06b206413
Joshua Haupt <joshua@hauptj.com>
2018-02-18 02:28:51 -0600

    started, need to add timeout for reboots

---
+++ roles/cloud/files/devmode.ps1
@@ -0,0 +1,11 @@
+# Enables Developer Mode
+
+# Source: https://stackoverflow.com/questions/40033608/enable-windows-10-developer-mode-programmatically
+# Create AppModelUnlock if it doesn't exist, required for enabling Developer Mode
+$RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+if (-not(Test-Path -Path $RegistryKeyPath)) {
+    New-Item -Path $RegistryKeyPath -ItemType Directory -Force
+}
+
+# Add registry value to enable Developer Mode
+New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
